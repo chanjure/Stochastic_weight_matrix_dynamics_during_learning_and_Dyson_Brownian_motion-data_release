92b93162108b67de541bae322681d0da20983902
chanjure <chanjure@snu.ac.kr>
2024-08-13 06:54:04 +0900

    Added comments in scripts

--- scripts/submit_train-scalar_phase.sh
+++ scripts/submit_train-scalar_phase.sh
@@ -1,9 +1,9 @@
 SRBMDIR=/Users/chanju/Dropbox/Lab/swansea/workspace/Git/SRBM/ # Change this to your SRBM directory
 BINDIR=../src/
-PNAME=SRBM-scalar
-BS=16
-LR=0.1
-NSEED=10
-EP=10
+PNAME=SRBM-scalar # Project name. The data will be saved in ./models/${PNAME}
+BS=16 # Batch size
+LR=0.1 # Learning rate
+NSEED=10 # Change the size of ensemble accordingly
+EP=10 # Change the number of epochs accordingly
 
 nohup python $BINDIR/check_mp_scalar.py --rbm_path ${SRBMDIR} --lr ${LR} --nseed ${NSEED} --bs ${BS} --epochs ${EP} --pname ${PNAME} > ../logs/${PNAME}-output.txt 2>&1 &
--- scripts/submit_train-tsmodel.sh
+++ scripts/submit_train-tsmodel.sh
@@ -1,8 +1,8 @@
 BINDIR=../src/
-PNAME=TSmodel
-BS=16
-LR=0.1
-NS=0.1
-NSEED=10
+PNAME=TSmodel # Project name. The data will be saved in ./models/${PNAME}
+BS=16 # Batch size
+LR=0.1 # Learning rate
+NS=0.1 # Strength of the induced noise
+NSEED=10 # Size of the ensemble
 
 nohup python $BINDIR/check_mp_ts.py --lr ${LR} --nseed ${NSEED} --bs ${BS} --ns ${NS} --pname ${PNAME} > ../logs/TSmodel-output.txt &
